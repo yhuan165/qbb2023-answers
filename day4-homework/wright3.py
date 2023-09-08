83f7094a00bfa07774cce16f39aaec54a0dd311b
Betty <yhuan165@jhu.edu>
2023-09-08 14:04:17 -0400

    answer for exercise 3 part 2

--- day4-homework/wright3.py
+++ day4-homework/wright3.py
@@ -18,6 +18,7 @@ def wrightfisher(af,pop):
 
 fig, ax = plt.subplots()
 
+'''
 avgtimelist = []
 popsize = []
 for i in range(5):
@@ -31,17 +32,32 @@ for i in range(5):
 	avgtime = sum(tfixation)/len(tfixation)
 	avgtimelist.append(avgtime)
 
-#ax.hist(tfixation)
 ax.scatter(popsize,avgtimelist)
 ax.set_xlabel("Population Size")
 ax.set_ylabel("Average Time to Fixation")
-#ax.set_ylim(0,1)
-#ax.set_xlim(0)
 ax.set_title("Population effect on time to fixation")
 fig.savefig( "ChangingPopulation.png" )
+'''
+
+avgtimelist = []
+freq = []
+for i in range(1000):
+	randomnum = np.random.uniform(0,1)
+	freq.append(randomnum)
+	tfixation = []
+	for i in range(50):
+		results = wrightfisher(randomnum, 1000)
+		tfixation.append(results[1])
+
+	avgtime = sum(tfixation)/len(tfixation)
+	avgtimelist.append(avgtime)
+
+ax.scatter(freq,avgtimelist)
+ax.set_xlabel("Allele Frequency")
+ax.set_ylabel("Average Time to Fixation")
+ax.set_title("Allele Frequency effect on time to fixation")
+fig.savefig( "ChangingFrequency.png" )
+
 
 plt.show()
 
-#	#x_positions = range(0,results[1])
-	#y_positions = results[0]
-	#ax.plot(x_positions, y_positions)
